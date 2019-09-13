3cf362badfdb3b54f21ecb6a559cd7e75058ab0e
aprilcoffee <8765498@gmail.com>
2019-09-13 15:46:26 +0800

    upload current broadcast files

--- server/public/sketch.js
+++ server/public/sketch.js
@@ -1,15 +1,23 @@
 var socket;
 var inputX,inputY;
+var num;
+var ip;
 function setup() {
  createCanvas(windowWidth,windowHeight);
  background(0);
-
- socket = io.connect('192.168.2.115:9527');
+ num = 0;
+ ip = '';
+ socket = io.connect('192.168.4.3:9527');
  socket.on('newText',newDrawing);
+ socket.on('assignNumber',assignNumber);
  textAlign(CENTER);
  textSize(50);
     inputX = 0;
     inputY = 0;
+}
+function assignNumber(data){
+    num = int(data.total);
+    ip = data.ip.slice(7); 
 }
 function newDrawing(data){
     inputX = data.x;
@@ -18,6 +26,16 @@ function newDrawing(data){
 }
 
 function draw() {
-     background(0);
+    background(0);
+    textSize(50);
+    textAlign(CENTER);
+    fill(255);
+    text(num,width/2,height/2-50);
+    textSize(20);
+    text(ip,width/2,height/2+50);
+     if(inputY == num){
      ellipse(width/2,height/2,inputX,inputX);
+     }else if(inputY==-1){
+    ellipse(width/2,height/2,inputX,inputX);
+     }
 }
